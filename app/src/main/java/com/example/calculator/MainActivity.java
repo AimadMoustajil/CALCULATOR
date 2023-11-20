88e80ed03276d5811d2930dbb59963e4764eaa2c
Aimad_Mousatjil <AimadMousatjilDev@gamil.com>
2023-11-20 16:02:05 +0100

    change title activety 1

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTitle("My First APP");
+        setTitle("My First APP on appstore");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
